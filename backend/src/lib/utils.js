ae01e1a20d0136b7a7e7061e01d29b0b4d136d8f
Dhrubajyoti Talukdar <dhrubajyoti.talukdar@siemens.com>
2025-06-19 15:04:54 +0530

    utils modified

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -9,6 +9,18 @@ export const hashPassword = async (password) => {
     const salt = await bcrypt.genSalt(10);
     return await bcrypt.hash(password, salt);
 }
+
+// Access token is used to access protected routes
+export const createAccessToken = (userId) => {
+    return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "15m" });
+}
+
+// Refresh token is used to get a new access token when the access token expires
+export const createRefreshToken = (userId) => {
+    return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
+}
+
+
 // export const generateToken = (userId, res) => {
 //     const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '' 
 //     });
