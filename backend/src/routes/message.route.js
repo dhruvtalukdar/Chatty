c3db2128880c9f00e9230ac311fc1504813ef583
Dhrubajyoti Talukdar <dhrubajyoti.talukdar@siemens.com>
2025-06-19 15:04:12 +0530

    Message routes updated

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/users", getUsersForSidebar);
-router.get("/:id", getMessages);
-router.post("/send/:id", sendMessage);
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
